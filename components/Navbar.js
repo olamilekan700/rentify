4c2293d3ed8df5598deb7018e9a80243634cfd26
olamilekan700 <adamhammed550@gmail.com>
2025-09-12 14:49:52 +0100

    changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,45 +1,44 @@
-    "use client"
+     "use client"
 import Link from "next/link";
 import { useState } from "react";
 import { CgProfile } from "react-icons/cg";
 import { RxHamburgerMenu } from "react-icons/rx";
 
-
 export function Navbar () {
     const [dropDown,setDropDown] = useState(false);
 
-    const toggleMenu = ()=>{
-        setDropDown(!dropDown)
+    const toggleDropDown = () =>{
+        setDropDown(!dropDown);
     }
     return(
-        <main className="bg-white shadow shadow-gray-300 w-full">
-            <div className="flex justify-between h-[50px] pt-3 px-5">
+        <main className="bg-white shadow shadow-gray-300 w-full sticky top-0">
+            <div className="flex justify-between h-[50px] pt-3 px-5 ">
                 <p className="text-blue-500 font-bold text-2xl">Rentify</p>
-                <ul className="hidden md:flex gap-10 font-semibold cursor-pointer ">
-                    <Link href="/"><li>Home</li></Link>
+                <ul className="hidden md:flex gap-10 font-semibold cursor-pointer">
+                   <Link href="/"><li>Home</li></Link>
                     <Link href="/dashboard/tenant-form"><li>Tenant-Form</li></Link>
-                    <li>Rent-List</li>
+                    <Link href="/dashboard/rent-list"><li>Rent-List</li></Link>
+                    
                 </ul>
                 <div className="hidden md:flex gap-3">
-                <p className="w-[60px] h[30px] bg-blue-500 text-white flex justify-center items-center rounded">Login</p>
-                <CgProfile className="text-3xl"/>
+                   <Link href="/auth/login"><p className="w-[60px] h-[30px] bg-blue-500 text-white flex justify-center items-center rounded  ">Login</p></Link>
+                   <Link href="/dashboard/profile"><CgProfile className="text-3xl"/></Link>
                 </div>
                 <div className="block md:hidden">
-                    <RxHamburgerMenu onClick={toggleMenu} className="text-blue-500 text-3xl"/>
+                    <RxHamburgerMenu onClick={toggleDropDown} className="text-blue-500 text-3xl" />
                 </div>
             </div>
-            {dropDown && (
-            <div className="px-5 pb-1 md:hidden">
+             {dropDown && (
+             <div className="px-5 pb-1 flex flex-col gap-2 md:hidden">
                 <ul>
-                    <li>Home</li>
-                    <li>tenant-form</li>
-                    <li>Rent-List</li>
+                    <Link href="/"><li>Home</li></Link>
+                    <Link href="/dashboard/tenant-form"><li>Tenant-Form</li></Link>
+                    <Link href="/dashboard/rent-list"><li>Rent-List</li></Link>
                 </ul>
-                <p className="w-[60px] h[30px] bg-blue-500 text-white flex justify-center items-center rounded">Login</p>
+                 <Link href="/auth/login"><p className="w-[60px] h-[30px] bg-blue-500 text-white flex justify-center items-center rounded  ">Login</p></Link>
                 <CgProfile className="text-3xl"/>
-            </div>
-            )}
+             </div>
+             )}
         </main>
-    
     )
 }
